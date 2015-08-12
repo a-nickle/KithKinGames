11c847991d2f3cf7eb7f27386622c7042646a878
a-nickle <a.nickle9001@gmail.com>
2015-08-12 13:23:54 -0400

    Andrew Commit
    Added ability base class

---
+++ KithKinGame/Assets/Scripts/AbilityBase.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbilityBase : MonoBehaviour 
+{
+	protected float damage;
+	
+	protected float cooldown;
+	
+	abstract protected void Activate();
+}
