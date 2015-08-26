0e543a89db4f2e8210239635772f11a3264c9080
a-nickle <a.nickle9001@gmail.com>
2015-08-26 16:18:52 -0400

    Andrew Commit
    Got the player to rotate based on the last position of the joystick.
    Right joystick now affects the players rotation, and left joystick now
    affects the players movement.

--- KithKinGame/Assets/Scripts/PlayerMovement.cs
+++ KithKinGame/Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
-{
+{	
 	void FixedUpdate()
 	{
 		MovePlayer();
+		RotatePlayer ();
 	}
 	
 	/// <summary>
@@ -16,13 +17,26 @@ public class PlayerMovement : MonoBehaviour
 		float fwdmove;
 		float sdemove;
 		
-		fwdmove =  Input.GetAxis("Vertical") * Constants.PLAYER_MOVE_SPEED * Time.deltaTime;
-		
-		sdemove = Input.GetAxis("Horizontal") * (-Constants.PLAYER_MOVE_SPEED) * Time.deltaTime;
+		//fwdmove = -Input.GetAxis("RotationY");
+		fwdmove = Input.GetAxis("Vertical") * Constants.PLAYER_MOVE_SPEED * Time.deltaTime;
+		sdemove = Input.GetAxis("Horizontal") * -Constants.PLAYER_MOVE_SPEED * Time.deltaTime;
+		//sdemove = -Input.GetAxis("RotationX");
 		
 		// Move forward and rotate based on input.
 		transform.Translate(Vector3.forward * fwdmove);
 		transform.Translate(Vector3.left * sdemove);
+	}
+	
+	private void RotatePlayer()
+	{
+		float xAxis = Input.GetAxis("RotationX");
+		float yAxis = -Input.GetAxis("RotationY");
+		
+		if(xAxis != 0.0f || yAxis != 0.0f)
+		{
+			float angle = Mathf.Atan2(yAxis, xAxis) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(90.0f - angle, Vector3.up);
+		}
 	}
 	
 }
